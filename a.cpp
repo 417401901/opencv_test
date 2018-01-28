c04cd76d9abaadd365bd405795ebb518fcc1589c
417401901 <wx1998@hotmail.com>
2018-01-28 12:44:28 +0800

    ok

--- a.cpp
+++ a.cpp
@@ -3,3 +3,32 @@
 
 using namespace std;
 using namespace cv;
+
+int g_ValudeSize = 1;
+int size,x,y;
+
+void on_filter(int , void *);
+
+Mat scrImage = imread("a.jpg");
+
+int main()
+{
+    namedWindow("各种滤波",1);
+    createTrackbar("内核大小", "各种滤波", &g_ValudeSize, 10, on_filter);
+    createTrackbar("x", "各种滤波", &x, 10, on_filter);
+    createTrackbar("y", "各种滤波", &y, 10, on_filter); 
+
+    on_filter(g_ValudeSize,0);
+
+    waitKey();
+
+    return 0;
+}
+
+void on_filter(int, void*)
+{
+    Mat dstImage(scrImage.size(),scrImage.type());
+    size = g_ValudeSize*2 + 1;
+    GaussianBlur(scrImage, dstImage,Size(size,size),x,y);
+    imshow("各种滤波",dstImage);
+}
